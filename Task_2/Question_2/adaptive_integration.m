11c30ef19385d08947be55f771e14d6a7e2c6956
q8526 <q8526@LUT5675.win.lut.fi>
2019-02-21 16:51:30 +0200

    Final version

--- Task_2/Question_2/adaptive_integration.m
+++ Task_2/Question_2/adaptive_integration.m
@@ -1,24 +1,40 @@
 % Afaptive integration by Midpoint method or Trapezoidal method
-%function calculate_integral = adaptive_integration(f, a, b, method)
-clc
-clear all
-close all
-
-    f = @(x) x^2;
-    a = 0;
-    b = 2;
-    method = -1;
-    n = 5;
+function result = adaptive_integration(f, a, b, eps, method)
     
-    if method == 1;
-        method_midpont = midpoint(f, a, b, n);
-    elseif method == 0;
-        method_trapezoidal = trapezoidal(f, a, b, n);
+    c = 1;
+    n(1,c) = 1;
+    initial_integration = 0;
+    
+    if method == 1
+        result(1, c) = midpoint(f, a, b, n(1,c));
+        error = result(1) - initial_integration;
+        while eps < error
+            c = c+1;
+            n(1, c) = n(1,c-1)*2;
+            result(1, c) = midpoint(f, a, b, n(1,c));
+            error(1, c) = abs(result(c) - result(c-1));       
+        end
+          
+    elseif method == 0
+        result(1,c) = trapezoidal(f, a, b, n(1,c)); 
+        error = result(1) - initial_integration;
+        while eps < error
+            c = c+1;
+            n(1, c) = n(1,c-1)*2;
+            result(1, c) = trapezoidal(f, a, b, n(1,c));
+            error(1, c) = abs(result(c) - result(c-1)); 
+        end  
+        
     elseif 0 > method
         fprintf('Method is not correct, your method input method is: %.1f, incorrect method. \n ', method) 
     elseif 1 < method
         fprintf('Method is not correct, your method input method is: %.1f, incorrect method. \n ', method) 
     end    
-   
     
-%end
+    fprintf('Result is: %.10f and Number of elements are: %.0f and Error is: %.10f.\n', result(c), n(c), error(c))
+    
+    semilogy (n,error)
+    xlabel('Numbers of elements')
+    ylabel('Error')
+    ylim([1e-10 1e-1])
+ end
--- Task_2/Question_2/main_file.m
+++ Task_2/Question_2/main_file.m
@@ -1,9 +1,13 @@
 %Main file for calcute integration
+clc
+clear all
+close all
 
-f = @(x) x^2;
+f = @(x) x^(1/2);
 a = 0;
 b = 2;
-method = 1;
+method = input('Select to method for calculate -> 1 is Midpoint, 0 is Trapezoidal:');
+eps = input('Select to Error for calculate:');
 
-result = adaptive_integration(f, a, b, method)
+results = adaptive_integration(f, a, b, eps, method);
 
