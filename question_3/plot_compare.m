6834a10c28fe7757c1fba659a07ac97da446223e
q8526 <q8526@LUT5682.win.lut.fi>
2019-02-28 18:36:20 +0200

    adam

--- question_3/plot_compare.m
+++
@@ -1,9 +0,0 @@
-function plot_compare(f,N,M,b)
-    t = linspace(-pi,pi,M);
-
-    [sn] = sinesum(t,b);
-    plot(t,f(t))
-    hold on
-    plot(t,sn)
-end
-
--- question_3/test_sinesum.m
+++
@@ -1,8 +0,0 @@
-function sn_1 = test_sinesum(t,b)
-
-sn_test(1,1) = b(1,1)*sin(1*t(1,1))+b(1,2)*sin(2*t(1,1));
-sn_test(1,2) = b(1,1)*sin(1*t(1,2))+b(1,2)*sin(2*t(1,2));
-
-sn_1=sn_test;
-
-end
