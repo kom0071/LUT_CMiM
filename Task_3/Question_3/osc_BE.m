c2fdf917102388f38f0e9ec3da4a21cef793d3fd
q8526 <q8526@LUT5675.win.lut.fi>
2019-03-09 15:18:55 +0200

    Task_3 - Question_3

---
+++ Task_3/Question_3/osc_BE.m
@@ -0,0 +1,26 @@
+clc
+close all
+
+omega = 5;
+P = 2*pi/omega;
+dt = P/2000;
+T = 3*P;
+N_t = floor(T/dt);
+t = linspace(0, N_t*dt, N_t+1);
+u = zeros(N_t+1, 1);
+v = zeros(N_t+1, 1);  
+
+% Initial condition
+X_0 = 2;
+u(1) = X_0;
+v(1) = 0;
+
+% Step equations forward in time
+for n = 2:N_t
+    v(n) = (v(n-1) - dt*omega^2*u(n-1))/(1+dt^2*omega^2);
+    u(n) = (u(n-1) + dt*v(n-1))/(1+dt^2*omega^2);
+end
+
+plot(t, u, 'b-', t, X_0*cos(omega*t), 'r--');
+legend('numerical', 'exact','location','northwest');
+xlabel('t');
