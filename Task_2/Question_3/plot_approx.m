b9c040be27bdabf793bdddab6420b4ef01a15fbd
komin <komin@158.196.194.16>
2019-03-12 15:54:31 +0200

    comment

--- Task_2/Question_3/plot_approx.m
+++ Task_2/Question_3/plot_approx.m
@@ -1,11 +1,11 @@
 function plot_approx(f, N, M, filename)
-    b = integrate_coeffs(f, N, M);
+    b = integrate_coeffs(f, N, M); % Call integration functio for calculet ceff. b
     t = linspace(-pi,pi,M);
 
-    [sn] = sinesum(t,b);
-    plot(t,f(t))
+    [sn] = sinesum(t,b); % Sinesum function
+    plot(t,f(t)) % Plot original function
     hold on
-    plot(t,sn)
+    plot(t,sn) % Plot aproximation function
     title('Plot originally function with approximation function')
     print(filename,'-dpdf')
     print(filename,'-depsc')
